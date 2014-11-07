3c9f58ccd6fe58ef14a833d6931adbf4b8f5c8f7
Joe Schaal <joe@schaal.net>
2014-11-07 08:38:47 -0500

    Morning work

--- course project part 1.R
+++ course project part 1.R
@@ -9,3 +9,21 @@ xfit<-seq(min(sim),max(sim),length=1000)
 yfit<-dnorm(xfit,mean=mean(sim),sd=sd(sim)) 
 yfit <- yfit*diff(simHist$mids[1:2])*length(sim) 
 lines(xfit, yfit, col="blue", lwd=2)
+
+#code block 1
+lambda <- 0.2
+simMean <- 1/lambda
+print(simMean)
+
+#code block 2
+set.seed(5555)
+simTrial <- mean(rexp(40,lambda))
+simTrial
+
+#code block 3
+trials <- 1000
+sim <- replicate(n=trials,expr = mean(rexp(n,lambda)))
+simHist <- hist(sim,breaks="Scott")
+
+#code block 4
+simHist <- hist(sim,breaks="Scott")
