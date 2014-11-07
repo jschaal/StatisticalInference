c5409b7d46aab49f668cc1a9d6e8821873934310
Joe Schaal <joe@schaal.net>
2014-11-07 16:49:05 -0500

    Refined

--- course project part 1.R
+++ course project part 1.R
@@ -1,7 +1,7 @@
 lambda <- 0.2
 n <- 40
 
-sim <- replicate(n=1000,expr = mean(rexp(n,lambda)))
+sim <- replicate(n=1000,expr = mean(rexp(40,lambda)))
 simHist <- hist(sim,breaks="Scott")
 
 #h<-hist(g, breaks=10, density=10, col="lightgray", xlab="Accuracy", main="Overall") 
@@ -48,3 +48,5 @@ xfit<-seq(min(sim),max(sim),length=1000)
 yfit<-dnorm(xfit,mean=mean(sim),sd=sd(sim)) 
 yfit <- yfit*diff(simHist$mids[1:2])*length(sim) 
 lines(xfit, yfit, col="blue", lwd=2)
+
+
