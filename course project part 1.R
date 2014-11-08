9d01a855dddcce2c749d29520832e2fce32f4c2f
Joe Schaal <joe@schaal.net>
2014-11-08 15:10:44 -0500

    Completed draft.

--- course project part 1.R
+++ course project part 1.R
@@ -17,30 +17,67 @@ print(expMean)
 
 #code block 2
 set.seed(5555)
-simTrial <- mean(rexp(40,lambda))
+n <- 40
+simTrial <- mean(rexp(n,lambda))
 print(simTrial)
 
 #code block 3
 trials <- 1000
-sim <- replicate(n=trials,expr = mean(rexp(n,lambda)))
+sim <- apply(matrix(rexp(n*trials,lambda),trials),1,mean)
+simMean <- mean(sim)
 simHist <- hist(sim,breaks="Scott",main = "Histogram of Simulation")
+yMax <- max(simHist$counts)
+meanText <- paste("mean = ",as.character(format(mean(sim),digits=4)))
+points(simMean,yMax,col="red",cex=2,pch=21,bg="red")
+text(x = simMean+1 ,y = yMax, meanText)
 
-#code block 4
+
+#code block 5
 simMean <- mean(sim)
 print(simMean)
 
-#code block 5
+#code block 6
 simSD <- sd(sim)
 print(simSD)
 
-#code block 7 & 8
+#code block7 
 simHist <- hist(sim,breaks="Scott",main = "Histogram of Simulation")
 xfit<-seq(min(sim),max(sim),length=1000) 
 yfit<-dnorm(xfit,mean=mean(sim),sd=sd(sim)) 
 yfit <- yfit*diff(simHist$mids[1:2])*length(sim) 
 lines(xfit, yfit, col="blue", lwd=2)
+points(simMean,yMax,col="red",cex=2,pch=21,bg="red")
+text(x = simMean+1 ,y = yMax, meanText)
 
-#code block 7
+#code block 8
+sim <- apply(matrix(rexp(n*trials,lambda),trials),1,mean)
+simHist <- hist(sim,breaks="Scott",main = "Histogram of Simulation")
+xfit<-seq(min(sim),max(sim),length=1000) 
+yfit<-dnorm(xfit,mean=mean(sim),sd=sd(sim)) 
+yfit <- yfit*diff(simHist$mids[1:2])*length(sim) 
+lines(xfit, yfit, col="blue", lwd=2)
+yMax <- max(simHist$counts)
+meanText <- paste("mean = ",as.character(format(mean(sim),digits=4)))
+points(simMean,yMax,col="red",cex=2,pch=21,bg="red")
+text(x = simMean+1 ,y = yMax, meanText)
+
+#code block 9
+trials <- 10000
+sim <- apply(matrix(rexp(n*trials,lambda),trials),1,mean)
+simHist <- hist(sim,breaks="Scott",main = "Histogram of Simulation")
+xfit<-seq(min(sim),max(sim),length=1000) 
+yfit<-dnorm(xfit,mean=mean(sim),sd=sd(sim)) 
+yfit <- yfit*diff(simHist$mids[1:2])*length(sim) 
+lines(xfit, yfit, col="blue", lwd=2)
+yMax <- max(simHist$counts)
+meanText <- paste("mean = ",as.character(format(mean(sim),digits=4)))
+points(simMean,yMax,col="red",cex=2,pch=21,bg="red")
+text(x = simMean+1 ,y = yMax, meanText)
+
+
+
+
+#old code block 7
 trials <- 10000
 sim <- replicate(n=trials,expr = mean(rexp(n,lambda)))
 simHist <- hist(sim,breaks="Scott",main = "Histogram of Simulation")
@@ -50,3 +87,9 @@ yfit <- yfit*diff(simHist$mids[1:2])*length(sim)
 lines(xfit, yfit, col="blue", lwd=2)
 
 
+set.seed(1)
+n <- 40
+trials <- 1000
+hist(runif(trials))
+sim <- apply(matrix(runif(n*trials),trials),1,mean)
+hist(sim)
